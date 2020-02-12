b276969820083ecb36229180fd80006e1f5020bc
Princess Itaya <taycornelius@att.net>
2020-02-11 21:41:44 -0500

    Updates

---
+++ precache-manifest.14ad6d3dc144a27dcc04447b7ddc8627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63908fa88fdeaef58b6349e0a2bad47",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "523f5bbc76297f3c497f",
+    "url": "/React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f0c81a451af74e85376",
+    "url": "/React/static/js/2.600fcfc2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React/static/js/2.600fcfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523f5bbc76297f3c497f",
+    "url": "/React/static/js/main.ee8ff182.chunk.js"
+  },
+  {
+    "revision": "64782ea8ce03851f1af3",
+    "url": "/React/static/js/runtime-main.07ecda4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  }
+]);
